c38679b7a107d539dc629abe23102143e66b364f
dayannealcantara <dayannealcantara14@gmail.com>
2024-01-10 12:41:51 -0300

    feat: passing detailed sales data, sales style page

---
+++ src/Components/VendaItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { IVenda } from '../Context/DataContext'
+
+export const VendaItem = ({venda} : {venda: IVenda}) => {
+  return (
+    <div className="venda box">
+      <a href="" style={{ fontFamily: "monospace" }} >
+        {venda.id}
+      </a>
+      <div>{venda.nome}</div>
+      <div>
+        {venda.preco.toLocaleString('pt-br', {style:'currency', currency:'BRL'})}
+      </div>
+    </div>
+  )
+}
