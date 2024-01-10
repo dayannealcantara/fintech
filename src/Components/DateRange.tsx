fb9c1bf9caf3d4e6bbaab8231c65ed416ab49103
dayannealcantara <dayannealcantara14@gmail.com>
2024-01-09 22:04:35 -0300

    feat: creating dataInput and style component

---
+++ src/Components/DateRange.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useData } from "../Context/DataContext";
+import  DateInput  from "./DateInput";
+
+const DateRange = () => {
+  const { inicio, setInicio, final, setFinal } = useData();
+
+  return (
+    <form className="box flex" onSubmit={(e) => e.preventDefault()}>
+      <DateInput
+        label="Início"
+        value={inicio}
+        onChange={({ target }) => setInicio(target.value)}
+      />
+    
+      <DateInput
+        label="Final"
+        value={final}
+        onChange={({ target }) => setFinal(target.value)}
+      />
+     
+    </form>
+  );
+};
+
+export default DateRange;
